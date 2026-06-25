57db0190cb5f5e98706fbd68c226e901fd49315e
Zaisc_ <zaidaansaad01@gmail.com>
2026-06-25 11:29:44 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,17 +1,2 @@
 # Remove Pixel kernel header
 rm -rf hardware/google/pixel/kernel_headers/Android.bp
-
-# Clone vendor tree
-git clone https://github.com/alternoegraha/vendor_xiaomi_fog vendor/xiaomi/fog
-
-# Clone kernel tree
-git clone --depth=1 -b fog-r-oss-staging https://github.com/alternoegraha/fog_kernel_xiaomi_sm6225 kernel/xiaomi/fog
-
-# Clone LineageOS' hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
-
-# Kernel fixups
-cd kernel/xiaomi/fog
-rm -rf Android.bp
-rm -rf techpack/audio/Android.bp
-cd ../../..
