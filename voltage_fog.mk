e68b3c0d052324ae1f2e11c364b4c4266c8f514d
Zaisc_ <zaidaansaad01@gmail.com>
2026-06-25 11:27:47 +0700

    Update to voltage_fog.mk

--- voltage_fog.mk
+++ voltage_fog.mk
@@ -8,16 +8,20 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Voltage stuff.
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
+
+# Voltage flags
 TARGET_BOOT_ANIMATION_RES := 720
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := false
+VOLTAGE_MAINTAINER := Zaisc_
+VOLTAGE_BUILD_TYPE := UNOFFICIAL
 
 # Inherit from fog device
 $(call inherit-product, device/xiaomi/fog/device.mk)
 
-PRODUCT_NAME := lineage_fog
+PRODUCT_NAME := voltage_fog
 PRODUCT_DEVICE := fog
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
